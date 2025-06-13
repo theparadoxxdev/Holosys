aa0f81843f57b7338d6197e93f6bbf195206cad9
Holly <36353656+theparadoxxdev@users.noreply.github.com>
2025-06-12 20:23:00 -0700

    Added main.vb form

---
+++ Holosys/main.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Trebuchet MS", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(956, 16)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(104, 22)
+        Label1.TabIndex = 0
+        Label1.Text = "Logged in as:"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Red
+        Button1.Location = New Point(1221, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(32, 32)
+        Button1.TabIndex = 1
+        Button1.Text = "X"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' main
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlText
+        ClientSize = New Size(1264, 681)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        ForeColor = SystemColors.Control
+        FormBorderStyle = FormBorderStyle.None
+        Name = "main"
+        Text = "Holosys Client"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Holosys/main.vb
@@ -0,0 +1,8 @@
+﻿Public Class main
+
+    Dim loggedinusr As String = login.txtUsername.Text
+
+    Private Sub main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
